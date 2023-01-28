8e7d27ae1a79b59febe5a642c08c0681c1c31215
rdvraval19 <76606536+rdvraval19@users.noreply.github.com>
2023-01-28 16:54:45 +0530

    Update Read.py

--- Assignment 2- File Operations/Read/Read.py
+++ Assignment 2- File Operations/Read/Read.py
@@ -1,2 +1,11 @@
+#r read
+#r+ read and write 
 with open("new.txt") as f:
   print(f.read())
+f2=open("new2.txt","r+")
+f2.write("\nHello World")
+print(f2.read())
+lines=("\nHow \nare \nyou?")
+f2.writelines(lines)
+print(f2.read())
+f2.close()
